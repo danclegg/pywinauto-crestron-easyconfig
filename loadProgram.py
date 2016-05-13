5cf09fa1cccd9044a3ed826aed02dc68821dea76
ydan <dclegg_github@byu.edu>
2016-05-13 11:10:18 -0600

    Adding loadProgram.py

---
+++ loadProgram.py
@@ -0,0 +1,63 @@
+#####
+#
+# Title: loadProgram.py
+# Author: Dan Clegg
+# Copyright: 2016, Dan Clegg
+# LICENSE: Apache 2.0
+#
+#####
+import sys
+import argparse
+import csv
+import glob,os
+import string
+
+from pywinauto.application import Application
+#import re
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--csv')
+
+args = parser.parse_args()
+csvFile = args.csv
+hosts = {}
+
+projectDir=os.path.abspath(os.getcwd())
+
+import csv
+with open(csvFile, 'r') as f:
+    reader = csv.reader(f)
+    hosts = dict((rows[0].strip(),rows[1].strip()) for rows in reader)
+
+#for item in list:
+#    hostname = item["hostname"]
+#    hostname = hostname.strip()
+#
+#    ip = item["ip"]
+#    ip = ip.strip()
+#
+#    hosts[hostname] = ip
+
+for host in hosts:
+    app = Application().Start(cmd_line=u'"C:\\Program Files (x86)\\Crestron\\Toolbox\\Toolbox.exe" ')
+    installerstoolboxappclass = app.InstallersToolboxAppClass
+    installerstoolboxappclass.Wait('ready', timeout=30)
+    button = installerstoolboxappclass.Button2
+    button.Click()
+    windowsformswindowappa = installerstoolboxappclass[u'6']
+    #windowsformswindowappa.Click()
+    button2 = installerstoolboxappclass.Button2
+    button2.Click()
+
+    window = app.top_window_()
+    window.Click()
+
+    window.TypeKeys(host.ip)
+    window.TypeKeys("{ENTER}")
+    window.Wait('ready',timeout=30)
+
+    window.TypeKeys("{TAB 5}")
+    window.TypeKeys("{SPACE}")
+    app.top_window_().DrawOutline()
+
+    app.Kill_()
